49136207e47fc85949ee085335df9d0c9e0d7295
Sirish Kanukunta <sirishkanukunta@Sirishs-MacBook-Pro.local>
2019-04-04 19:11:09 -0400

    merger

--- helloWorld.py
+++ helloWorld.py
@@ -1,8 +1,7 @@
 print("hello world")
 print("This is sirish's contribution")
-<<<<<<< HEAD
 print("project 1")
-=======
 
 print("Testing testing 1, 2, 3")
->>>>>>> a12f1c4b526c943532c6438ceeae9fb2cb7017f7
+
+print("please merge me")
